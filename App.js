3a87ac9fb4d8b4812347a6c940f7c4cc8449f9d5
priya <priyadharsini3097@gmail.com>
2024-05-08 18:35:46 +0530

    drawer tab navigator update

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from './Components/Home';
 import Details from './Components/Details';
 import Subscribition from './Components/Subscribition';
@@ -10,10 +11,12 @@ import User from './Components/User';
 function TabNavigator(){
 const Tab = createBottomTabNavigator();
 return(
-   <Tab.Navigator>
-      <Tab.Screen name='Main' component={StackNavigator}/>
+   <Tab.Navigator initialRouteName='drawer' screenOptions={{headerShown:false}}>
+       <Tab.Screen name='Main' component={StackNavigator}/> 
+      {/* <Tab.Screen name='Home' component={Home}/> */}
       <Tab.Screen name='Subscribition' component={Subscribition}/>
       <Tab.Screen name='Help' component={Help}/>
+      <Tab.Screen name='drawer' component={MyDrawer}/>
      </Tab.Navigator>
     );
 }
@@ -27,6 +30,21 @@ function StackNavigator(){
       <Stack.Screen name='User' component={User}/>
       </Stack.Navigator>
    );
+}
+function MyDrawer() {
+   const Drawer = createDrawerNavigator();
+
+   return(
+
+      <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Details" component={Details} />
+      <Drawer.Screen name="User" component={User} />
+      <Drawer.Screen name="Subscribition" component={Subscribition} />
+      <Drawer.Screen name="Help" component={Help} />
+    </Drawer.Navigator>
+   )
+
 }
 export default  function App (){
    return(
